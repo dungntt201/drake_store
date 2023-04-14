501f635ddad3d21fbb4a55ef3f3fb2b56c3cfd66
dungntt201 <dung.ntt992001@gmail.com>
2023-04-14 10:06:15 +0700

    update upload folder root

--- drake-store/src/main/java/com/dev/drakestore/dto/Constants.java
+++ drake-store/src/main/java/com/dev/drakestore/dto/Constants.java
@@ -1,5 +1,5 @@
 package com.dev.drakestore.dto;
 
 public interface Constants {
-	public String UPLOAD_FOLDER_ROOT="/TAI LIEU HOC TAP/Drake Store/drake-store/Uploads/";
+	public String UPLOAD_FOLDER_ROOT="/TAI LIEU HOC TAP/drake_store/drake-store/Uploads/";
 }
